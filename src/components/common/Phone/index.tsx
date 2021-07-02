5af17cd2d91d144af5f9fbfc65c6cc96890a3795
Jamie Joao Mendoza Sernaqué <jojham1994@gmail.com>
2021-07-02 02:29:24 -0500

    [update] PhoneBorder

--- src/components/common/Phone/index.tsx
+++ src/components/common/Phone/index.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react'
 
 import styles from './Phone.module.sass'
-// import PhoneMock from 'assets/images/phone-mock-copy.png'
 
 interface Props {
   children?: ReactNode
@@ -10,8 +9,6 @@ interface Props {
 export const Phone = ({ children }: Props) => {
   return (
     <div className={styles.phone}>
-      {/* <img src={PhoneMock} alt='Mockup de teléfono móvil' /> */}
-
       <div className={styles.phone_container}>
         {children}
       </div>
